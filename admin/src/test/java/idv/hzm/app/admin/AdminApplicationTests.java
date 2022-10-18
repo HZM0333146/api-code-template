750ef9d26f54c1100108aec5be858d8289c6a5a8
ZiMin <guang83328@gmail.com>
2022-10-19 06:51:45 +0800

    add admin module

---
+++ admin/src/test/java/idv/hzm/app/admin/AdminApplicationTests.java
@@ -0,0 +1,15 @@
+package idv.hzm.app.admin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import idv.hzm.app.admin.controller.CoinController;
+
+@SpringBootTest
+class AdminApplicationTests {
+  
+	@Test
+	void contextLoads() {}
+
+}
